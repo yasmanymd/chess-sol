e8360f2f87084e51ce09f5bdc40da3e7274fb03a
Yasmany Molina Diaz <yasmany@gmail.com>
2018-10-09 22:53:18 -0400

    heroku deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as io from 'socket.io-client';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+const socket = io();
+socket.on('hello', (data: { message: string }) => alert(data.message));
 
 ReactDOM.render(
   <App />,
---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import express from 'express';
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/', (req, resp, next) => res.sendFile(__dirname + './index.html'));
+
+io.on('connection', socket => socket.emit('hello', {message: 'hello from server.'}));
+
+server.listen(port);
