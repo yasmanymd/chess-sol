601b8878e06f8bcd59434fdb5f0493803fcab879
Yasmany Molina Diaz <yasmany@gmail.com>
2018-11-23 00:22:56 -0500

    Fix socket.io address

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as io from 'socket.io-client';
 import App from './App';
 import './index.css';
 
-const socket = io('http://localhost:5100/');
+const socket = io('https://chess-yas.herokuapp.com/');
 window['io'] = {};
 window['io'].socket = socket;
 
