a32c639e850510998f865bb531f0e427a9687140
Yasmany Molina Diaz <yasmany@gmail.com>
2018-12-04 22:00:54 -0500

    Fix url

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as io from 'socket.io-client';
 import App from './App';
 import './index.css';
 
-const socket = io('https://chess-yas.herokuapp.com/');
+const socket = io('http://chess-yas.com/');
 window['io'] = {};
 window['io'].socket = socket;
 
